453184ef0b0be8579b02f358fde13ae16b54b887
King-Hur Wu <king2510@gmail.com>
2023-12-19 17:10:37 -0600

    set up webpack

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use('/dist', express.static(path.join(__dirname, '../dist')));
+// serve index.html on the route '/'
+app.get('/', (req, res) => {
+  return res.status(200).sendFile(path.join(__dirname, '../index.html'));
+});
+
+app.listen(3000);
