f6b87307da2fbbe9b3a4a0bb8564340f66e6f263
King-Hur Wu <king2510@gmail.com>
2023-12-21 19:04:53 -0600

    added affirmation

--- src/components/Eat.jsx
+++ src/components/Eat.jsx
@@ -3,7 +3,6 @@ import '../styles.css';
 import { Macros } from './Macros.jsx';
 import { Header } from './Header.jsx';
 
-
 export const Eat = (props) => {
     return (
     <div>
--- src/components/Wins.jsx
+++ src/components/Wins.jsx
@@ -4,14 +4,20 @@ import { Header } from './Header.jsx';
 
 export const Wins = (props) => {
     
+    const [affirmation, setAffirmation] = useState('your dad is proud of you')
+    let aff = ''
+    const onAffClick = () => {
+        setAffirmation(aff)
+        
+      }
     return (
     <>
         <Header />
         <div className="login-container">
-            <h2>your dad is proud of you</h2>
+            <h2>{affirmation}</h2>
             <form className='form' method='POST' action='/login'>
-            <input name='username' type='text' placeholder='i didnt cry today &#129402;'></input>
-            <button className='loginBtn' type='submit'>good job me! &#129395;</button>
+            <input name='username' onChange={(e)=> aff = e.target.value} type='text' placeholder='i didnt cry today &#129402;'></input>
+            <button className='loginBtn' input type='reset' onClick ={onAffClick}>good job me! &#129395;</button>
             </form>
         </div>
     </>
